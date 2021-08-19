bdb714f832c4618437c05e72dfe80944b5bbe2e6
Shivali Raju Bandi <40134659+ShivaliBandi@users.noreply.github.com>
2021-08-19 20:30:08 +0530

    Create 203.RemoveElementFromLinkedList.cpp

---
+++ 203.RemoveElementFromLinkedList.cpp
@@ -0,0 +1,70 @@
+/*
+203. Remove Linked List Elements
+
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50
+
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(!head)
+            return NULL;
+        else
+        {
+            ListNode *temp=head->next,*prev=head;
+            while(head!=NULL && head->val==val)
+                head=head->next;
+          
+           while(temp!=NULL)
+           {
+               while(temp->val==val &&  temp->next!=NULL)
+               {
+                    prev->next=temp->next;
+                   temp=temp->next;
+               }
+                if(temp->val==val)
+                    prev->next=temp->next;
+               prev=temp;
+               temp=temp->next;
+               
+           }
+            return head;
+        }
+    }
+};
